2ae8a78a9c67cc800f7aff57651a953842b28bc5
Danila Poyarkov <dev@dannote.net>
2026-09-04 11:58:08 +0300

    Upgrade to Unhead 3

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import { createHead } from '@unhead/vue/client'
+import { ValidatePlugin } from 'unhead/plugins'
 import { routes, handleHotUpdate } from 'vue-router/auto-routes'
 
 import './assets/main.css'
@@ -16,9 +17,13 @@ if (import.meta.hot) {
   handleHotUpdate(router)
 }
 
+const head = createHead({
+  plugins: import.meta.env.DEV ? [ValidatePlugin()] : []
+})
+
 const app = createApp(App)
 
-app.use(createHead())
+app.use(head)
 app.use(router)
 
 app.mount('#app')
